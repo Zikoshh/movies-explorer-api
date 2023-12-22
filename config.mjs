b5b8437cd9dc9af113a7ca0dac11da3e02e64c67
Tilek <zikoshh89@gmail.com>
2023-12-22 14:34:51 +0700

    Update config.mjs

--- config.mjs
+++ config.mjs
@@ -53,7 +53,7 @@ export const validationConfig = {
     }),
   }),
   createUserValidation: celebrate({
-    body: Joi.object.keys({
+    body: Joi.object().keys({
       name: Joi.string().required().min(2).max(30),
       email: Joi.string().required().email(),
       password: Joi.string().required(),
